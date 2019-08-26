3c867ca119a111e8e7190f512810e4a3eb5fbdaf
Sayali Upasani <sayaliupasani@gmail.com>
2019-08-26 14:24:44 -0700

    Changed the data storage from dictionary to Redis

--- app/status_app.py
+++ app/status_app.py
@@ -3,26 +3,35 @@ from flask import Flask, request, render_template
 import requests
 from requests.exceptions import ConnectionError, MissingSchema
 import time
+import redis
 
 app = Flask(__name__)
 url_dict = {}
 
+
+r = redis.Redis(host='localhost',port=6379,password='baba1612', charset='utf-8', decode_responses=True)
+print(r.keys())
+
 def requesturl(processed_url):
-    url_dict[processed_url] = {}
+    #url_dict[processed_url] = {}
     urlcheck = requests.get(processed_url)
     code = urlcheck.status_code
-    url_dict[processed_url]['code']= code
-    url_dict[processed_url]['time']=int(time.time())
+    epoch = int(time.time())
+    timestamp = time.ctime(epoch)
+    url_for_redis = processed_url.replace(".", "#")
+    r.hmset(url_for_redis, {"code":code, "time": timestamp, "epoch":epoch})
+
     #url _dict[processed_url] = [int(time.time()), code]
     if code == 200:
         #print('This is requesturl code 200 block')
-        url_dict[processed_url]['availability']='The site is available'
-        return render_template('webpage.html', url = processed_url,  status=code, availability = 'The website you specified is availble!', cache = \
-                               url_dict.items())
+        #url_dict[processed_url]['availability']='The site is available'
+        r.hmset(url_for_redis, {"message":"The site is available as per last timestamp"})
+        return render_template('webpage.html', url = processed_url,  status=r.hget(url_for_redis, "code"), availability = r.hget(url_for_redis, "message"), \
+            timestamp = r.hget(url_for_redis, "time"), cache = r.hgetall(url_for_redis))
     else:
-        url_dict[processed_url]['availability']='The site returned {}'.format(code)
-        return render_template('webpage.html', url = processed_url, status=code,
-                               availability = 'The website you specified returned {}!'.format(code), cache = url_dict.items())
+        r.hmset(url_for_redis, {"message":"The site returned a non-200 code"})
+        return render_template('webpage.html', url = processed_url, status=r.hget(url_for_redis, "code"),
+                               availability = r.hget(url_for_redis, "message"), timestamp = r.hget(url_for_redis, "time"), cache = r.hgetall(url_for_redis))
     
 @app.route('/') #If the user comes on /path, it gets redirected to webpage.htm saved under templates directory
 def webpage():
@@ -32,12 +41,14 @@ def webpage():
 def web_url():
     url = request.form['url']
     processed_url = url.lower()
+    url_for_redis = processed_url.replace(".","#")
     try:
-        if processed_url in url_dict:
-            if url_dict[processed_url]['time']- int(time.time()) > 600:
+        if r.hexists(url_for_redis, "code"):
+            if int(time.time()) - int(r.hget(url_for_redis, "epoch")) > 600:
                 return requesturl(processed_url)
             else:
-                return render_template('webpage.html', url = processed_url, status=url_dict[processed_url]['code'], availability = url_dict[processed_url]['availability'], cache = url_dict.items())
+                return render_template('webpage.html', url = processed_url, status=r.hget(url_for_redis, "code"), availability = r.hget(url_for_redis, "message"), \
+                    timestamp = r.hget(url_for_redis, "time"), cache = r.hgetall(url_for_redis))
         else:
             return requesturl(processed_url)
     except ConnectionError:
@@ -47,4 +58,5 @@ def web_url():
 
 if __name__=='__main__':
     app.run(host='0.0.0.0')
+
 
